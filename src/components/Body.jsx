cd6c479995d05dc196c935b934aae86ebdb7927c
Fernando Guerrero <devrrior@gmail.com>
2022-02-27 23:32:35 -0600

    create notes components

---
+++ src/components/Body.jsx
@@ -0,0 +1,10 @@
+
+const Body = () => {
+  return (
+    <div>
+      <h3>Body</h3>
+    </div>
+  )
+}
+
+export default Body
---
+++ src/pages/NotesListPage.jsx
@@ -0,0 +1,15 @@
+import notes from '../assets/data';
+
+const NotesListPage = () => {
+  return (
+    <div>
+      <div className='notes-list'>
+        {notes.map((note) => (
+          <p>{note.body}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NotesListPage;
